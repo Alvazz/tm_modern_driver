b9ba8dd55523675ebc8da550f64809da97955920
s880367 <s880367@gmail.com>
2017-05-24 22:00:43 +0800

    add license

--- tm_modern_driver.cpp
+++ tm_modern_driver.cpp
@@ -1,3 +1,22 @@
+/*********************************************************************
+ * tm_modern_driver.cpp
+ *
+ * Copyright 2017 Copyright National Chiao Tung University, ISCI lab.
+ * Author: Howard Chen (s880367@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **********************************************************************/
+
 #include "src/tm_driver/tm_driver.h"
 
 #include <termios.h>
